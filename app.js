dd7dbfa1111423396b8b4d4d8b6302de71627464
Jaime Farfan <jfarfan@utec.edu.pe>
2020-07-01 19:10:44 -0500

    producto, modelo, vista y controlador

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var bodyParser = require('body-parser');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var usuariosRouter = require('./routes/usuarios');
+var productosRouter = require('./routes/productos');
 
 var app = express();
 
@@ -33,6 +34,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/usuarios', usuariosRouter);
+app.use('/productos', productosRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
