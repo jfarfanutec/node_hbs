8b605b05f521436447fe2d1080e92fb4e8c92aaf
Jaime Farfan <jfarfan@utec.edu.pe>
2020-07-01 20:00:26 -0500

    Update, roter, controlador y vista

--- controllers/ProductoController.js
+++ controllers/ProductoController.js
@@ -44,4 +44,41 @@ productoController.show = function(req, res) {
     });
 };
 
+productoController.edit = function(req, res) {
+    Producto.findOne({ _id: req.params.id }).exec(function(err, producto) {
+        if( err ) {
+            console.log('Error: ',err);
+            return;
+        }
+        res.render('../views/producto/edit',{producto: producto});
+    });
+};
+
+productoController.delete = function(req, res) {
+    Producto.remove({ _id: req.params.id }).exec(function(err) {
+        if( err ) {
+            console.log('Error: ',err);
+            return;
+        }
+        console.log("Usuario eliminado");
+        res.redirect("/productos");
+    });
+};
+
+productoController.update = function(req, res){
+    Producto.findByIdAndUpdate( req.params.id, {$set: {
+        nombre: req.body.nombre,
+        precio: req.body.precio,
+        stock: req.body.stock
+    }}, { new: true },
+    function( err, producto){
+        if( err ){ 
+            console.log('Error: ', err); 
+            res.render('../views/producto/edit', {producto: req.body} );
+        }
+        console.log( producto );
+        res.redirect('/productos/show/' + producto._id);
+    });
+};
+
 module.exports = productoController;
--- routes/productos.js
+++ routes/productos.js
@@ -5,9 +5,9 @@ var producto = require('../controllers/ProductoController');
 
 router.get('/',producto.list);
 router.get('/show/:id',producto.show);
-// router.get('/edit/:id',producto.edit);
-// router.post('/delete/:id',producto.delete);
-// router.post('/update/:id',producto.update);
+router.get('/edit/:id',producto.edit);
+router.post('/delete/:id',producto.delete);
+router.post('/update/:id',producto.update);
 router.get('/create',producto.create);
 router.post('/save',producto.save);
 
